3b05b0a2d141a15171c03ec395843006b3c4f4b8
Aleix <aleixclemente@gmail.com>
2023-01-15 00:21:15 +0100

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,132 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Aleix Clemente",
+  jobDescription: "Web Developer",
+  about: `A front-end engineer from Barcelona. I am currently learning Astro and Typescript, and expanding my knowledge of Nextjs. Apart from web development, my other passions are enduro, football and chess.
+  `,
+
+  experience: [
+    {
+      name: "OASYS, SL",
+      location: "Barcelona",
+      startDate: "March 2021",
+      endDate: "Present",
+      link: "https://oasys-sw.com/",
+      description: [
+        "- Front-End Developer. Building web-apps for several companies in the pharmaceutical and food industry",
+        "- Working daily with React, Javascript, Semantic/Mui and apexcharts among other technologies",
+      ],
+    },
+
+    {
+      name: "DECATHLON SPAIN, S.A.",
+      location: "El Prat de Llobregat",
+      startDate: "2018",
+      endDate: "2020",
+      description: [
+        "- Logistics Operator and IT Technician",
+        "- (Hardware, network, scripting...)",
+        "- Small dashboard website built with PHP",
+      ],
+    },
+
+    {
+      name: "Non technical jobs",
+      location: "Barcelona and surroundings",
+      startDate: "2012",
+      endDate: "2016",
+      description: [
+        "- Warehouse waiter at Mercabarna",
+        "- Soccer coach in lower categories",
+        "- After-school activity monitor of sports initiation",
+        "- Offset printing operator",
+      ],
+    },
+  ],
+
+  volunteering: [
+    {
+      name: "International Cooperation Volunteering in Tujereng, Gambia",
+      location: "Gambia",
+      startDate: "2017",
+      endDate: "2017",
+      description: ["- Documentary recording and editing (2017)"],
+    },
+  ],
+
+  education: [
+    {
+      name: "Linkia FP",
+      location: "Barcelona",
+      startDate: "2018",
+      endDate: "2020",
+      link: "https://linkiafp.es/fp-grado-superior-desarrollo-de-aplicaciones-web-a-distancia/",
+      description: ["Web Apps Development Certificate of Higher Education"],
+    },
+    {
+      name: "UAB - Autonomous University of Barcelona",
+      location: "Bellaterra",
+      startDate: "2013",
+      endDate: "2017",
+      link: "https://www.uab.cat/web/facultat-de-ciencies-de-la-comunicacio-1345702085095.html",
+      description: ["1st, 2nd and 3rd course - Journalism University Degree"],
+    },
+    {
+      name: "IES Frederic Mompou",
+      location: "Sant Vicenç dels Horts",
+      startDate: "2010",
+      endDate: "2012",
+      link: "https://agora.xtec.cat/iesfmompou/",
+      description: ["Bachelor of Science and Technology"],
+    },
+    {
+      name: "IES Frederic Mompou",
+      location: "Sant Vicenç dels Horts",
+      startDate: "2010",
+      endDate: "2012",
+      link: "https://agora.xtec.cat/iesfmompou/",
+      description: ["Certificate of Secondary Education"],
+    },
+  ],
+  addOns: [
+    {
+      name: "Car and Motorcycle License",
+      description: ["- Own vehicle"],
+    },
+    {
+      name: "Microsoft utilities",
+      description: ["- Word", "- Excel", "- PowerPoint"],
+    },
+  ],
+  socialMedia: {
+    github: "https://github.com/aleixdeveloper",
+    email: "aleixclemente@gmail.com",
+    linkedin: "https://www.linkedin.com/in/aleixclemente/",
+    instagram: "https://www.instagram.com/aleeiixxx/",
+  },
+
+  projects: [
+    {
+      title: "Music band store",
+      isFeatured: true,
+      thumbnail: "/assets/images/fenok-store/fenok-store.jpg",
+      githubUrl:
+        "https://github.com/aleixdeveloper/nextjs-ecommerce-tuto/tree/main",
+      liveUrl: "https://fenokstore.vercel.app/",
+    },
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
