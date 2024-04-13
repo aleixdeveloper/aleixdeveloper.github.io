778431775de88bc889c70c6e01347bccf5c55b89
Aleix <aleixclemente@gmail.com>
2024-04-13 16:56:29 +0200

    fix theme toggle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  darkMode: ["class", '[data-theme="dark"]'],
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        dark: "#111111",
-        light: "#FFFFFF",
-        blue: "#33d2ff",
-        gray: "#C4C4C4",
-        "dark-gray": "#1A1A1A",
-      },
-      fontFamily: {
-        /*   inter: ["Inter", "sans-serif"], */
-        outfit: ["Outfit", "sans-serif"],
-      },
-    },
-    screens: {
-      "2xs": "350px",
-      xs: "500px",
-      ...defaultTheme.screens,
-    },
-  },
-  plugins: [],
-};
+	darkMode: "class",
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				"dark": "#111111",
+				"light": "#FFFFFF",
+				"blue": "#33d2ff",
+				"gray": "#C4C4C4",
+				"dark-gray": "#1A1A1A",
+			},
+			fontFamily: {
+				/*   inter: ["Inter", "sans-serif"], */
+				outfit: ["Outfit", "sans-serif"],
+			},
+		},
+		screens: {
+			"2xs": "350px",
+			"xs": "500px",
+			...defaultTheme.screens,
+		},
+	},
+	plugins: [],
+}
