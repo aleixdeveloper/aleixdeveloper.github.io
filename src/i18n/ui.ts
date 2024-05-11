50a9afee416f8bbe7115809bcc49a1d0e4ab879e
Aleix <37262637+aleixdeveloper@users.noreply.github.com>
2024-05-11 13:53:59 +0200

    language selector fixes

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,54 +5,85 @@ import En from "@/assets/icons/english-flag.webp"
 // Add missing imports
 export const LANGUAGES: Record<
 	string,
-	{ code: string; name: string; symbol: string; flag: ImageMetadata }
-> = {
-	ca: {
-		symbol: "🇨🇦",
-		code: "ca",
-		name: "Català",
-		flag: Cat,
-	},
-	en: {
-		symbol: "🇪🇳",
-		code: "en",
-		name: "English",
-		flag: En,
-	},
-	es: {
-		symbol: "🇪🇸",
-		code: "es",
-		name: "Español",
-		flag: Es,
-	},
-}
+	{
+		code: string
+		name: string
+		symbol: string
+		flag: ImageMetadata
+	}
+> =
+	{
+		ca: {
+			symbol:
+				"🇨🇦",
+			code:
+				"ca",
+			name:
+				"Català",
+			flag:
+				Cat,
+		},
+		en: {
+			symbol:
+				"🇪🇳",
+			code:
+				"en",
+			name:
+				"English",
+			flag:
+				En,
+		},
+		es: {
+			symbol:
+				"🇪🇸",
+			code:
+				"es",
+			name:
+				"Español",
+			flag:
+				Es,
+		},
+	}
 
-export const defaultLang = "es"
-export const showDefaultLang = false
+export const defaultLang =
+	"en"
+export const showDefaultLang =
+	false
 
-export const ui = {
-	es: {
-		"nav.inicio": "Inicio",
-		"nav.proyectos": "Proyectos",
-	},
-	en: {
-		"nav.inicio": "Home",
-		"nav.proyectos": "Projects",
-	},
-	ca: {
-		"nav.inicio": "Inici",
-		"nav.proyectos": "Projectes",
-	},
-} as const
+export const ui =
+	{
+		es: {
+			"nav.inicio":
+				"Inicio",
+			"nav.proyectos":
+				"Proyectos",
+		},
+		en: {
+			"nav.inicio":
+				"Home",
+			"nav.proyectos":
+				"Projects",
+		},
+		ca: {
+			"nav.inicio":
+				"Inici",
+			"nav.proyectos":
+				"Projectes",
+		},
+	} as const
 
-export const routes = {
-	es: {
-		proyectos: "proyectos",
-	},
-	en: {
-		proyectos: "projects",
-	},
-	ca: {
-		proyectos: "projectes",
-	},
-}
+export const routes =
+	{
+		es: {
+			proyectos:
+				"proyectos",
+		},
+		en: {
+			proyectos:
+				"projects",
+		},
+		ca: {
+			proyectos:
+				"projectes",
+		},
+	}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,26 +2,74 @@
 
 const defaultTheme = require("tailwindcss/defaultTheme")
 
-module.exports = {
-	darkMode: "class",
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			colors: {
-				"dark": "#111111",
-				"light": "#FFFFFF",
-				"dark-gray": "#1A1A1A",
+module.exports =
+	{
+		darkMode:
+			"class",
+		content:
+			[
+				"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+			],
+		theme:
+			{
+				extend:
+					{
+						colors:
+							{
+								"gallery":
+									{
+										50: "#f7f6f5",
+										100: "#eeedec",
+										200: "#d6d4d2",
+										300: "#bcb9b5",
+										400: "#a19a96",
+										500: "#8e8581",
+										600: "#817875",
+										700: "#6c6462",
+										800: "#5a5352",
+										900: "#4a4644",
+										950: "#272423",
+									},
+								"shark":
+									{
+										50: "#f7f7f8",
+										100: "#efeef0",
+										200: "#d9d8df",
+										300: "#b7b6c3",
+										400: "#908fa1",
+										500: "#737186",
+										600: "#5d5b6e",
+										700: "#4d4a5a",
+										800: "#42404c",
+										900: "#3a3842",
+										950: "#1e1d22",
+									},
+								"dark":
+									"#1E1D22",
+								"light":
+									"#EEEDEC",
+								"dark-gray":
+									"#1A1A1A",
+							},
+						fontFamily:
+							{
+								/*   inter: ["Inter", "sans-serif"], */
+								outfit:
+									[
+										"Outfit",
+										"sans-serif",
+									],
+							},
+					},
+				screens:
+					{
+						"2xs":
+							"350px",
+						"xs":
+							"500px",
+						...defaultTheme.screens,
+					},
 			},
-			fontFamily: {
-				/*   inter: ["Inter", "sans-serif"], */
-				outfit: ["Outfit", "sans-serif"],
-			},
-		},
-		screens: {
-			"2xs": "350px",
-			"xs": "500px",
-			...defaultTheme.screens,
-		},
-	},
-	plugins: [],
-}
+		plugins:
+			[],
+	}
