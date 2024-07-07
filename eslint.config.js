d721facde6aa6a2005b3448e2860bb9f011d74b2
Aleix <aleixclemente@gmail.com>
2024-07-07 12:58:26 +0200

    eslint shit

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import config from "eslint-config-standard";
+
+/* export default [
+  ...[].concat(config),
+]; */
+
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    }
+  }
+];
